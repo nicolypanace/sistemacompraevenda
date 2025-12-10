16aa3f3a48c2bd814c3a8693d127203d6bcbeebd
Nicoly Panace <n.panace@aluno.ifsp.edu.br>
2025-12-09 22:46:23 -0300

    adicionar cadastro, conexão com banco de dados e criação de tablas via sql

--- sistema de compra e venda/Cadastro.Designer.cs
+++ sistema de compra e venda/Cadastro.Designer.cs
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtNomeFantasia = new System.Windows.Forms.TextBox();
+            this.txtRazaoSocial = new System.Windows.Forms.TextBox();
+            this.txtCNPJ = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(277, 374);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(260, 55);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "SALVAR";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNomeFantasia
+            // 
+            this.txtNomeFantasia.Location = new System.Drawing.Point(196, 89);
+            this.txtNomeFantasia.Name = "txtNomeFantasia";
+            this.txtNomeFantasia.Size = new System.Drawing.Size(462, 20);
+            this.txtNomeFantasia.TabIndex = 1;
+            // 
+            // txtRazaoSocial
+            // 
+            this.txtRazaoSocial.Location = new System.Drawing.Point(196, 135);
+            this.txtRazaoSocial.Name = "txtRazaoSocial";
+            this.txtRazaoSocial.Size = new System.Drawing.Size(462, 20);
+            this.txtRazaoSocial.TabIndex = 2;
+            // 
+            // txtCNPJ
+            // 
+            this.txtCNPJ.Location = new System.Drawing.Point(196, 186);
+            this.txtCNPJ.Name = "txtCNPJ";
+            this.txtCNPJ.Size = new System.Drawing.Size(462, 20);
+            this.txtCNPJ.TabIndex = 3;
+            this.txtCNPJ.TextChanged += new System.EventHandler(this.txtCNPJ_TextChanged);
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(196, 268);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(462, 20);
+            this.txtTelefone.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(196, 228);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(462, 20);
+            this.txtEmail.TabIndex = 5;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(196, 311);
+            this.txtEndereco.Multiline = true;
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(462, 24);
+            this.txtEndereco.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "NOME COMERC.:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "RAZÃO SOC.:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "CNPJ:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "EMAIL:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(127, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "TELEFONE:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(127, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ENDEREÇO";
+            // 
+            // Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtCNPJ);
+            this.Controls.Add(this.txtRazaoSocial);
+            this.Controls.Add(this.txtNomeFantasia);
+            this.Controls.Add(this.btnSalvar);
+            this.Name = "Cadastro";
             this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtNomeFantasia;
+        private System.Windows.Forms.TextBox txtRazaoSocial;
+        private System.Windows.Forms.TextBox txtCNPJ;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- sistema de compra e venda/Cadastro.cs
+++ sistema de compra e venda/Cadastro.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace sistema_de_compra_e_venda
 {
     public partial class Cadastro : Form
     {
+        string connectionString = @"Server=.\SQLEXPRESS;Database=SistemaComercial;Trusted_Connection=True;";
         public Cadastro()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtNomeFantasia.Text == "")
+            {
+                MessageBox.Show("O Nome Fantasia é obrigatório!");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string sql = @"INSERT INTO Empresa 
+                               (NomeFantasia, RazaoSocial, CNPJ, Telefone, Email, Endereco)
+                               VALUES
+                               (@NomeFantasia, @RazaoSocial, @CNPJ, @Telefone, @Email, @Endereco)";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@NomeFantasia", txtNomeFantasia.Text);
+                cmd.Parameters.AddWithValue("@RazaoSocial", txtRazaoSocial.Text);
+                cmd.Parameters.AddWithValue("@CNPJ", txtCNPJ.Text);
+                cmd.Parameters.AddWithValue("@Telefone", txtTelefone.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@Endereco", txtEndereco.Text);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Empresa cadastrada com sucesso!");
+
+            txtNomeFantasia.Clear();
+            txtRazaoSocial.Clear();
+            txtCNPJ.Clear();
+            txtTelefone.Clear();
+            txtEmail.Clear();
+            txtEndereco.Clear();
+        }
+
+        private void txtCNPJ_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
